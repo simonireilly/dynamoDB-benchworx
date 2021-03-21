8311615f4300b9d39a6508cae8a789e4395048d3
Simon Reilly <simon.reilly@infinityworks.com>
2021-03-21 10:36:40 +0000

    Finish testing roles for cypress

--- cypress/component/pages/credentials/forms/roles.spec.tsx
+++ cypress/component/pages/credentials/forms/roles.spec.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import { mount } from "@cypress/react";
 import { Roles } from "./../../../../../src/pages/credentials/forms/roles";
 
-describe("HelloWorld component", () => {
-  it("works", () => {
+describe("Role Component", () => {
+  beforeEach(() => {
     mount(<Roles />);
-    // now use standard Cypress commands
+  });
+  it("renders a select for available roles", () => {
     cy.contains("Choose AWS Account Role").should("be.visible");
+  });
+
+  it("allows direct input of an ARN for the role", () => {
+    cy.get("#aws-role-arn").type(
+      "arn:aws:iam::account-id:role/role-name-with-path"
+    );
   });
 });
